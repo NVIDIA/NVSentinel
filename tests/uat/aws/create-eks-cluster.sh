92d5178fb0d1b51066a49c50f343195a10144490
Mark Chmarny <mchmarny@nvidia.com>
2025-11-17 16:34:07 -0800

    chore: upgrade eksctl

--- tests/uat/aws/create-eks-cluster.sh
+++ tests/uat/aws/create-eks-cluster.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
+set -euox pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/../common.sh"
